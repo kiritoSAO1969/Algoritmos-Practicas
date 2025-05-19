fe49d3c15654a86388e0d376b4a7a8b2ef5d0eaa
zaddkielezio@gmail.com <jesus>
2025-05-19 17:09:53 -0600

    Version 1.0 Colocacion funciones de creacion, muestra y SumaMaxima de arreglos Ya funcional menu

--- Practica3/BibliotecaFunciones.h
+++ Practica3/BibliotecaFunciones.h
@@ -3,37 +3,71 @@
 #include <time.h>
 #include <windows.h>
 
-void llenarArreglo(int arr[], int n) {
-    if (n >= 0) {
-        printf("Ingresa el elemento %d: ", n + 1);
-        scanf("%d", &arr[n]);
-        llenarArreglo(arr, n-1);
+void mostrarArreglo(int * arreglo, int n, int i, int inicial, int final) {
+    //Llamada recursiva de impresion de arreglo
+    if (i < n) {
+        if (i==inicial){        //Personalizacion para corchete inicial, inicial es para marcar de donde inicia
+            printf(("["));
+            printf("%d ", arreglo[i]);
+            mostrarArreglo(arreglo, n, i + 1, inicial, final);      //Personalizacion para corchete final, final es el final del arreglo
+        } else if (i==final){
+            printf("%d", arreglo[i]);
+            printf("]");
+        } else {        //Caso comun de impresion
+            printf("%d ", arreglo[i]);
+            mostrarArreglo(arreglo, n, i + 1, inicial, final);
+        }
     }
 }
 
-void mostrar_arreglo(int *arreglo,int numero){
-	for (int i=0; i< numero; i++){
-		printf("%d ",*(arreglo+i));
-	}
-	printf("\n");;
+void llenarArreglo(int * arreglo, int n, int i) {
+    if (i < n) {
+        printf("Ingresa el elemento %d: ", i + 1);  //LLamada recursiva para llenar el arreglo
+        scanf("%d", &arreglo[i]);
+        llenarArreglo(arreglo, n, i + 1);
+    }
 }
 
-int suma_recursiva(int *arreglo, int inicio, int final) {
+void creacionArreglo(int ** arreglo, int * largoArreglo) {
+    printf("Iniciemos creando el arreglo\n");
+        printf("Ingrese el largo del arreglo a crear (en numero y MAX=20): ");          //Personalizacion largo del arrreglo
+        scanf("%d", largoArreglo);
+        fflush(stdin);
+
+        *arreglo = (int*)calloc((*largoArreglo), (sizeof(int)));           //Creacion arreglo de manera dinamica
+
+        if (*arreglo== NULL) {              //Chequeo en caso de error en la asignacion de memoria
+            printf("Memoria insuficiente para la generacion del arreglo\nVolviendo al menu....");
+            Sleep(5000);
+            return;
+        }
+
+        printf("Ahora procederemos con el llenado del arreglo\n");      //Procedimiento de llenado y muestra del arreglo
+        llenarArreglo(*arreglo, *largoArreglo, 0);
+        printf("Se ha creado el arreglo con exito es el siguiente: ");
+        mostrarArreglo(*arreglo, *largoArreglo, 0, 0, (*largoArreglo)-1);
+        printf("\n");
+        Sleep(5000);
+}
+
+int suma_recursiva(int *arreglo, int inicio, int final) { 
     if (inicio > final) {
         return 0;
     }
-    return arreglo[inicio] + suma_recursiva(arreglo, inicio + 1, final);
+    return arreglo[inicio] + suma_recursiva(arreglo, inicio + 1, final);    //Acumulacion de la suma de valores del arreglo
 }
 
 void comparacion(int *arreglo, int inicio_arreglo, int final_arreglo, int * best_value, int *subarrgl) {
     int valor;
-    if (inicio_arreglo==final_arreglo){
+
+    //Comparacion de valores de cada arreglo
+    if (inicio_arreglo==final_arreglo){                 //Caso en que solo queda un elemento
         if (arreglo[inicio_arreglo]>(*best_value)){
             (*best_value)=arreglo[inicio_arreglo];
             subarrgl[0]=inicio_arreglo;
             subarrgl[1]=final_arreglo;
         }
-    } else {
+    } else {                    //Caso en que el arreglo contiene mas de dos elementos
         valor=suma_recursiva(arreglo,inicio_arreglo,final_arreglo);
         if(valor>(*best_value)){
             (*best_value)=valor;
@@ -42,29 +76,52 @@ void comparacion(int *arreglo, int inicio_arreglo, int final_arreglo, int * best
         }  
     }
     
-    if(inicio_arreglo<final_arreglo) {
+    if(inicio_arreglo<final_arreglo) {      //Llamada recursva de la funcion
         int mitad=(inicio_arreglo+final_arreglo)/2;
         comparacion(arreglo,inicio_arreglo,mitad,best_value,subarrgl);
 
-        if (((inicio_arreglo+final_arreglo)%2)==0){
+        if (((inicio_arreglo+final_arreglo)%2)==0){                 //Caso numero par de elementos
             comparacion(arreglo,mitad,final_arreglo,best_value,subarrgl);
-        } else {
+        } else {                    //Caso numero impar de elementos
             comparacion(arreglo,mitad+1,final_arreglo,best_value,subarrgl);
         }
     }
 }
 
-void busqueda(int **arreglo,int *largo, int *menu) {
-    int inicio_opcional,final_opcional=*largo;
-    int subarregl[2];
-    int best_value=0;
-    printf("BUSQUEDA SUBARREGLO MAS ALTO\n");
+void busquedaSumMaxima(int **arreglo,int largo) {
+    int subarregl[2];   //Areglo que almacena posiciones mejor
+    int best_value=0;   //Variable almacenadora de mejor valor
+    comparacion(*arreglo,0,largo-1,&best_value,subarregl);      //Calculo de la suma maxima
     
-    printf("El arreglo original es: ");
-    mostrar_arreglo(*arreglo, *largo); 
+    system("cls");
+    printf("Se ha encontrado el mejor maximo\n");
+    
+    printf("RESULTADOS\n");
+    printf("El arreglo original es el siguiente: ");
+    mostrarArreglo(*arreglo, largo, 0, 0, largo-1);
 
-    comparacion(*arreglo,inicio_opcional,final_opcional-1,&best_value,subarregl);
-
-    printf("El mejor subarreglo son las siguientes coordenadas: %d %d\n",subarregl[0],subarregl[1]);
-    printf("El mejor valor es: %d\n",best_value);
+    printf("\n\nLa suma maxima del arreglo tiene las siguientes caracteristicas: \n");
+    printf("El valor es de: %d\n",best_value);
+    printf("Coordenadas del subarreglo: %d, %d\n",subarregl[0],subarregl[1]);
+    printf("Subarreglo: ");
+    mostrarArreglo(*arreglo, (subarregl[1])+1, subarregl[0], subarregl[0], subarregl[1]);
+    Sleep(5000);
 }
+
+void sumaMaxima() {
+    int * arreglo;
+    int largoArregl;
+
+    system("cls");
+    printf("Ha seleccionado la opcion de sumaMaxima del arreglo\n"
+            "Empezaremos creando el arreglo a trabajar\n");
+    creacionArreglo(&arreglo, &largoArregl);            //Creacion y llenado de arreglo
+    printf("Procediendo a realizar los calculos de la Suma Maxima del arreglo\n");
+    Sleep(3000); 
+    busquedaSumMaxima(&arreglo, largoArregl);           //Operacion de la busqueda maxima y muestra de resultados
+    free(arreglo);                                      //Liberacion de memoria
+    arreglo = NULL;
+}
+
+
+
--- Practica3/Practica3.c
+++ Practica3/Practica3.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
+#include "BibliotecaFunciones.h"
 
 void menu_recursi(int repeticiones) {
     int opcion_escoger;
 
     system("cls");
-    if (repeticiones>0){
+    if (repeticiones>0){                //Seleccion de opcion a escoger
         printf("MENU DE OPCIONES\n"
             "Escoga la operacion a realizar, de las siguientes opciones.\n"
             "1. Arreglo de Suma Maxima\n"
@@ -15,13 +12,27 @@ void menu_recursi(int repeticiones) {
             "3. Inversor de arreglo\n"
             "Opcion a escoger: ");
         scanf("%d", &opcion_escoger);
+        fflush(stdin);
+
+        switch (opcion_escoger)             //Switch para controlar opciones
+        {
+        case 1:
+            sumaMaxima();
+            break;
+        case 2:
+            break;
+        case 3: 
+            break;
+        default:
+            break;
+        }
+
         menu_recursi(repeticiones-1);
     } else {
-        printf("Opciones finalizadas, se ha acabado el programa");
+        printf("Opciones finalizadas, se ha acabado el programa");      //Finalizacion del programa
         return;
     }
 }
-
 
 
 int main() {
@@ -31,6 +42,7 @@ int main() {
     printf("Ingrese el numero de operaciones a realizar, con la finalidad de ver todos los ejercicios\n");
     printf("Numero de operaciones: ");
     scanf("%d",&numero_opciones);
+    fflush(stdin);
     printf(("Muchas gracias, prodeciendo al menu de operaciones"));
     Sleep(2);
     system("cls");
